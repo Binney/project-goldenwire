4460cef0f04905475484212e992ddd08b8446591
Harry Best <harrybest800@gmail.com>
2023-11-10 18:11:32 +0000

    Broadcast latitude

--- bluefruit/gps/gps.ino
+++ bluefruit/gps/gps.ino
@@ -57,33 +57,32 @@ uint32_t tilt = 0;
  * Heart Rate Measurement Char: 0x2A37
  * Body Sensor Location Char:   0x2A38
  */
-BLEService        hrms = BLEService(UUID16_SVC_HEART_RATE);
-BLECharacteristic hrmc = BLECharacteristic(UUID16_CHR_HEART_RATE_MEASUREMENT);
-BLECharacteristic bslc = BLECharacteristic(UUID16_CHR_BODY_SENSOR_LOCATION);
+BLEService        bleService = BLEService(0x0070);
+BLECharacteristic latitude = BLECharacteristic(0x0071);
 
 BLEDis bledis;    // DIS (Device Information Service) helper class instance
 BLEBas blebas;    // BAS (Battery Service) helper class instance
 
 uint8_t  bps = 0;
 
-void cccd_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_value)
-{
-    // Display the raw request packet
-    Serial.print("CCCD Updated: ");
-    //Serial.printBuffer(request->data, request->len);
-    Serial.print(cccd_value);
-    Serial.println("");
+// void cccd_callback(uint16_t conn_hdl, BLECharacteristic* chr, uint16_t cccd_value)
+// {
+//     // Display the raw request packet
+//     Serial.print("CCCD Updated: ");
+//     //Serial.printBuffer(request->data, request->len);
+//     Serial.print(cccd_value);
+//     Serial.println("");
 
-    // Check the characteristic this CCCD update is associated with in case
-    // this handler is used for multiple CCCD records.
-    if (chr->uuid == hrmc.uuid) {
-        if (chr->indicateEnabled(conn_hdl)) {
-            Serial.println("Temperature Measurement 'Indicate' enabled");
-        } else {
-            Serial.println("Temperature Measurement 'Indicate' disabled");
-        }
-    }
-}
+//     // Check the characteristic this CCCD update is associated with in case
+//     // this handler is used for multiple CCCD records.
+//     if (chr->uuid == latitude.uuid) {
+//         if (chr->indicateEnabled(conn_hdl)) {
+//             Serial.println("Temperature Measurement 'Indicate' enabled");
+//         } else {
+//             Serial.println("Temperature Measurement 'Indicate' disabled");
+//         }
+//     }
+// }
 
 void setupTilt()
 {
@@ -100,41 +99,12 @@ void setupHRM(void)
   // Heart Rate Measurement       0x2A37  Mandatory   Notify
   // Body Sensor Location         0x2A38  Optional    Read
   // Heart Rate Control Point     0x2A39  Conditional Write       <-- Not used here
-  hrms.begin();
-
-  // Note: You must call .begin() on the BLEService before calling .begin() on
-  // any characteristic(s) within that service definition.. Calling .begin() on
-  // a BLECharacteristic will cause it to be added to the last BLEService that
-  // was 'begin()'ed!
-
-  // Configure the Heart Rate Measurement characteristic
-  // See: https://www.bluetooth.com/specifications/gatt/viewer?attributeXmlFile=org.bluetooth.characteristic.heart_rate_measurement.xml
-  // Permission = Notify
-  // Min Len    = 1
-  // Max Len    = 8
-  //    B0      = UINT8  - Flag (MANDATORY)
-  //      b5:7  = Reserved
-  //      b4    = RR-Internal (0 = Not present, 1 = Present)
-  //      b3    = Energy expended status (0 = Not present, 1 = Present)
-  //      b1:2  = Sensor contact status (0+1 = Not supported, 2 = Supported but contact not detected, 3 = Supported and detected)
-  //      b0    = Value format (0 = UINT8, 1 = UINT16)
-  //    B1      = UINT8  - 8-bit heart rate measurement value in BPM
-  //    B2:3    = UINT16 - 16-bit heart rate measurement value in BPM
-  //    B4:5    = UINT16 - Energy expended in joules
-  //    B6:7    = UINT16 - RR Internal (1/1024 second resolution)
-  hrmc.setProperties(CHR_PROPS_NOTIFY | CHR_PROPS_BROADCAST );
-  hrmc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  hrmc.setFixedLen(2);
-  hrmc.setCccdWriteCallback(cccd_callback);  // Optionally capture CCCD updates
-  hrmc.begin();
-  uint8_t hrmdata[2] = { 0b00000110, 0x40 }; // Set the characteristic to use 8-bit values, with the sensor connected and detected
-  hrmc.notify(hrmdata, 2);                 // Use .notify instead of .write!
-
-  bslc.setProperties(CHR_PROPS_READ);
-  bslc.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
-  bslc.setFixedLen(1);
-  bslc.begin();
-  bslc.write8(2);    // Set the characteristic to 'Wrist' (2)
+  bleService.begin();
+  latitude.setProperties(CHR_PROPS_NOTIFY);
+  latitude.setPermission(SECMODE_OPEN, SECMODE_NO_ACCESS);
+  latitude.setFixedLen(4);
+  latitude.setUserDescriptor("latitude");
+  latitude.begin();
 }
 
 void updateTilt()
@@ -159,20 +129,11 @@ void startAdv(void)
   Bluefruit.Advertising.addTxPower();
 
   // Include HRM Service UUID
-  Bluefruit.Advertising.addService(hrms);
+  Bluefruit.Advertising.addService(bleService);
 
   // Include Name
   Bluefruit.Advertising.addName();
   
-  /* Start Advertising
-   * - Enable auto advertising if disconnected
-   * - Interval:  fast mode = 20 ms, slow mode = 152.5 ms
-   * - Timeout for fast mode is 30 seconds
-   * - Start(timeout) with timeout = 0 will advertise forever (until connected)
-   * 
-   * For recommended advertising interval
-   * https://developer.apple.com/library/content/qa/qa1931/_index.html   
-   */
   Bluefruit.Advertising.restartOnDisconnect(true);
   Bluefruit.Advertising.setInterval(32, 244);    // in unit of 0.625 ms
   Bluefruit.Advertising.setFastTimeout(30);      // number of seconds in fast mode
@@ -190,12 +151,11 @@ void setup()
   Serial.println("Adafruit GPS library basic parsing test!");
   // Initialise the Bluefruit module
   Serial.println("Initialise the Bluefruit nRF52 module");
+  Bluefruit.setName("Vending Machine");
   Bluefruit.begin();
 
   // Configure and Start the Device Information Service
   Serial.println("Configuring the Device Information Service");
-  bledis.setManufacturer("Adafruit Industries");
-  bledis.setModel("Bluefruit Feather52");
   bledis.begin();
 
   // Start the BLE Battery Service and set it to 100%
@@ -282,13 +242,9 @@ void loop() // run over and over again
 
      if ( Bluefruit.connected() ) {
     Serial.println("CONNECTED");
-    uint8_t hrmdata[2] = { 0b00000110, bps++ };           // Sensor connected, increment BPS value
     
-    // Note: We use .notify instead of .write!
-    // If it is connected but CCCD is not enabled
-    // The characteristic's value is still updated although notification is not sent
-    if ( hrmc.notify(hrmdata, sizeof(hrmdata)) ){
-      Serial.print("Heart Rate Measurement updated to: "); Serial.println(bps); 
+    if ( latitude.notify32((uint32_t) GPS.latitude_fixed) ){
+      Serial.print("Latitude updated to: "); Serial.println(GPS.latitude_fixed); 
     }else{
       Serial.println("ERROR: Notify not set in the CCCD or not connected!");
     }
