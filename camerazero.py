f0bd6d67593dbef5cb46f838b4b91a861d3c160d
Sarah Binney <sarah.binney@softwire.com>
2023-11-10 14:40:33 +0000

    speculativeee

--- camerazero.py
+++ camerazero.py
@@ -1,15 +1,28 @@
 from picamera import PiCamera
 from time import sleep
+from gpiozero import Button
+import os.path
+from signal import pause
 
 camera = PiCamera()
 camera.start_preview(alpha=200)
 
 sleep(5)
 
+def next_available_filename():
+	file_num = 1
+	while os.path.isfile("test" + str(file_num) + ".jpg"):
+		file_num += 1
+	return "test" + str(file_num) + ".jpg"
+
 def snap():
-	filename = "images/test.jpg"
+	print("Click!")
+	filename = next_available_filename()
 	camera.capture(filename)
 	# TODO send via Slack
+	print("done")
 
-snap()
-camera.stop_preview()
+shutter = Button(2)
+shutter.when_pressed = snap
+
+pause()
